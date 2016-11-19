98eeee3d31ed743fe74d3448b5a804c4f690e2e7
Arondite <stephenknotek@gmail.com>
2016-11-18 18:53:35 -0700

    Started the pin work on the kiosk

--- CCSVolunteerMVC/Controllers/KioskController.cs
+++ CCSVolunteerMVC/Controllers/KioskController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CCSVolunteerMVC.DAL;
+using CCSVolunteerMVC.Models;
 
 namespace CCSVolunteerMVC.Controllers
 {
@@ -29,6 +30,30 @@ namespace CCSVolunteerMVC.Controllers
 		public ActionResult VolunteerClock()
 		{
 			return View(db.Volunteers.ToList());
+		}
+
+		//public ActionResult GroupClock()
+		//{
+		//	return View();
+		//}
+		public ActionResult GroupClock(char? alphabetChar)
+		{
+			List<VolunteerGroup> groupList = new List<VolunteerGroup>();
+			if (alphabetChar == null)
+			{
+				alphabetChar = 'A';
+			}
+			string alphabetString = alphabetChar.ToString();
+			string[] alphabet = { "A", "B", "C","D","E","F","G","H","I","J","K", "L",
+						"M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+			foreach (var item in alphabet)
+			{
+				if (item == alphabetString)
+				{
+					groupList = (db.VolunteerGroups.Where(g => g.volGrpName.Contains(item)).ToList());
+				}
+			}
+			return View(groupList);
 		}
     }
 }
